5446a86306b1d0ad0be3df9d38258b6dd87f4c1f
daviderivolta-ett <davide.rivolta@grupposcai.it>
2024-01-30 15:29:33 +0100

    Overlay and favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { BurgerComponent } from './components/burger/burger.component';
+import { OverlayComponent } from './components/overlay/overlay.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, BurgerComponent],
+  imports: [RouterOutlet, NavComponent, BurgerComponent, OverlayComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/overlay/overlay.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-overlay',
+  standalone: true,
+  imports: [],
+  templateUrl: './overlay.component.html',
+  styleUrl: './overlay.component.scss'
+})
+export class OverlayComponent {
+
+}
